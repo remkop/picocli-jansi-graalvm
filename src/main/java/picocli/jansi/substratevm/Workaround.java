563ad97d2e93822f2d54aceeb4b4ea9885c307f6
Remko Popma <remkop@yahoo.com>
2019-10-24 10:31:03 +0900

    Factor out the workaround to a separate class so it can be used with other Jansi classes than AnsiConsole

---
+++ src/main/java/picocli/jansi/substratevm/Workaround.java
@@ -0,0 +1,45 @@
+package picocli.jansi.substratevm;
+
+/**
+ * Enables the use of Jansi native library code in GraalVM native image applications.
+ * <p>
+ * This class provides a workaround for a problem in Jansi's
+ * <code>org.fusesource.hawtjni.runtime.Library</code>
+ * that prevents the native libraries embedded in the Jansi JAR in
+ * <code>/META-INF/native/*64/*</code> from being loaded
+ * when running in a GraalVM native image.
+ * </p>
+ * <p>
+ * Usage: call {@code Workaround.enableLibraryLoad()} before calling any Jansi code.
+ * For example:
+ * </p>
+ * <pre>
+ * import org.fusesource.jansi.AnsiConsole;
+ * import picocli.jansi.substratevm.Workaround;
+ *
+ * public class MyApp {
+ *     static {
+ *         Workaround.enableLibraryLoad();
+ *     }
+ *
+ *     public static void main(String[] args)  {
+ *         AnsiConsole.systemInstall();
+ *         doCoolStuff();
+ *         AnsiConsole.systemUninstall();
+ *     }
+ *     // ...
+ * }
+ * </pre>
+ */
+public class Workaround {
+    private Workaround() {}
+
+    // workaround for https://github.com/fusesource/jansi/issues/162
+    public static void enableLibraryLoad() {
+        String arch = System.getProperty("os.arch");
+        String vm = System.getProperty("java.vm.name");
+        if (arch.endsWith("64") && "Substrate VM".equals(vm)) {
+            System.setProperty("sun.arch.data.model", "64");
+        }
+    }
+}
