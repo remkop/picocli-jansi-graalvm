c30758fab9bf7c6e13767cb693c29904305546e2
Remko Popma <remkop@yahoo.com>
2019-10-25 01:05:47 +0900

    TEST wait for process to exit before obtaining exit code

--- examples/src/test/java/my/pkg/MyAppTest.java
+++ examples/src/test/java/my/pkg/MyAppTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.StreamSupport;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,7 +19,7 @@ class MyAppTest {
     }
 
     @Test
-    public void testUsageHelp() throws IOException {
+    public void testUsageHelp() throws IOException, InterruptedException {
         Process process = new ProcessBuilder(executable, "--help").start();
 
         String exected = String.format("" +
@@ -29,33 +30,36 @@ class MyAppTest {
                 "  -V, --version       Print version information and exit.%n");
         assertEquals(exected, getStdOut(process));
         assertEquals("", getStdErr(process));
+        process.waitFor(3, TimeUnit.SECONDS);
         assertEquals(0, process.exitValue());
     }
 
     @Test
-    public void testVersionInfo() throws IOException {
+    public void testVersionInfo() throws IOException, InterruptedException {
         Process process = new ProcessBuilder(executable, "--version").start();
 
         String exected = String.format("myapp 1.0%n"); // JVM: 1.8.0_222 (Oracle Corporation Substrate VM GraalVM dev)
 
         assertEquals(exected, getStdOut(process));
         assertEquals("", getStdErr(process));
+        process.waitFor(3, TimeUnit.SECONDS);
         assertEquals(0, process.exitValue());
     }
 
     @Test
-    public void testNormalOperation() throws IOException {
+    public void testNormalOperation() throws IOException, InterruptedException {
         Process process = new ProcessBuilder(executable, "--user", "me").start();
 
         String exected = String.format("Hello, me%n");
 
         assertEquals(exected, getStdOut(process));
         assertEquals("", getStdErr(process));
+        process.waitFor(3, TimeUnit.SECONDS);
         assertEquals(0, process.exitValue());
     }
 
     @Test
-    public void testInvalidInput() throws IOException {
+    public void testInvalidInput() throws IOException, InterruptedException {
         Process process = new ProcessBuilder(executable, "--unknown").start();
 
         String exected = String.format("" +
@@ -67,6 +71,7 @@ class MyAppTest {
                 "  -V, --version       Print version information and exit.%n");
         assertEquals(exected, getStdErr(process));
         assertEquals("", getStdOut(process));
+        process.waitFor(3, TimeUnit.SECONDS);
         assertEquals(2, process.exitValue());
     }
 
